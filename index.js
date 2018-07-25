e805ce99f61a649f355659db2186263590b47af9
AnveshMekala <36524965+ANVESH96@users.noreply.github.com>
2018-07-25 14:52:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+var request= require("request");
+var cheerio = require("cheerio");
+var split = require("split-string");
+var url= "https://www.cricbuzz.com/";
+request(url,function(err,response,html){
+    return gethtml(html)
+})
+function gethtml(html){
+    var $ = cheerio.load(html);
+    var div =$("div");
+    var element = $(div).find("nav.cb-nav>a.cb-hm-mnu-itm").attr('title')
+    console.log(element)
+}
